414668ccabe175c1a5574f3660417c557a7dd0e3
ikjoonp <junny.i.am@gmail.com>
2024-03-28 15:37:12 -0700

    Logic for rendering and some html changes.

--- pfm_react_frontend/src/App.js
+++ pfm_react_frontend/src/App.js
@@ -1,29 +1,54 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import ExpenseForm from './components/ExpenseForm';
 import ExpenseList from './components/ExpenseList';
 import Header from './components/Header';
-import ExpensePieChart from './components/Chart'
+import ExpensePieChart from './components/Chart';
+import ExpenseFilter from './components/ExpenseFilter';
+import { getUniqueMonths, sortExpensesByDate } from './utils/dateUtils';
 import './App.css';
 
 function App() {
   const [expenses, setExpenses] = useState([]);
+  const [filterOptions, setFilterOptions] = useState([]);
+  const [currentFilter, setCurrentFilter] = useState('all');
 
+  // First mount effects.
   useEffect(() => {
     // Fetch expenses from backend
     const fetchExpenses = async () => {
-      const result = await fetch('http://localhost:8080/api/expenses');
-      if (!result) {
-        throw new Error('Failed to fetch!');
+      // Captures exceptions related to initiating fetch request (network errors).
+      try {
+        const result = await fetch('http://localhost:8080/api/expenses');
+        // Handle HTTP response errors.
+        if (!result) {
+          throw new Error('Failed to fetch!');
+        } 
+        let data = await result.json();
+        data = sortExpensesByDate(data);
+        // Sets contents of Expense List on first mount.
+        setExpenses(data);
+      } catch(error) {
+        console.error("Error fetching expenses:",error);
       }
-      const data = await result.json();
-      setExpenses(data);
     };
+
     fetchExpenses();
   }, []);
 
-  
+  // Effect whenever 'expenses' changes (add/delete) and on first mount.
+  useEffect(() => {
+    // Set filter options.
+    const uniqueMonths = getUniqueMonths(expenses);
+    setFilterOptions(uniqueMonths);
+  }, [expenses]);
+
+  const handleFilterChange = (e) => {
+    setCurrentFilter(e.target.value);
+  }
+
   const handleAddExpense = (newExpense) => {
-    setExpenses((prevExpenses) => [...prevExpenses, newExpense]);
+    const updatedExpenses = sortExpensesByDate([...expenses, newExpense]);
+    setExpenses(updatedExpenses);
   };
 
   const handleDeleteExpense = async (id) => {
@@ -41,14 +66,26 @@ function App() {
     }
   };
 
+  // Returns an array of expenses that match the current filter option.
+  const filteredExpenses = expenses.filter(expense => {
+    // Directly compare the 'YYYY-MM' portion of the date strings
+    const expenseYearMonth = expense.date.substring(0, 7);
+    return currentFilter === 'all' ? true : expenseYearMonth === currentFilter;
+  });
   return (
     <div>
       <Header />
-      <ExpensePieChart expenses={expenses} />
-      <ExpenseForm onExpenseAdded = {handleAddExpense} />
-      <ExpenseList expenses = {expenses} onDeleteExpense = {handleDeleteExpense}/>
+      <div className="filter-and-form">
+        <ExpenseFilter filterOptions={filterOptions} currentFilter={currentFilter} onFilterChange={handleFilterChange}/>
+        <ExpenseForm onExpenseAdded={handleAddExpense}/>
+      </div>
+      <div className="chart-and-list">
+        <ExpensePieChart expenses={filteredExpenses}/>
+        <ExpenseList expenses={filteredExpenses} onDeleteExpense={handleDeleteExpense}/>
+      </div>
     </div>
   );
+  
 }
 
 export default App;
